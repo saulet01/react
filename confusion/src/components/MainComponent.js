6254fba22c3fe8e39ee9aab94e6891d05a91b4a8
Saulet Yskak <sauletik01@gmail.com>
2018-12-27 13:06:52 -0600

    Introduction to React Router, added Navbar and links to Footer

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -4,28 +4,32 @@ import {DISHES} from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
 import HeaderComponent from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 class MainComponent extends Component {
   constructor(props){
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null
     }
   }
 
-  onDishSelect(dishId){
-    this.setState({
-      selectedDish: dishId
-    });
-  }
-
   render() {
+    const HomePage = () => {
+      return(
+        <Home />
+      );
+    }
+
     return (
       <div>
           <HeaderComponent />
-          <Menu dishes = {this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
-          <DishDetail dish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+          <Switch>
+            <Route path="/home" component={HomePage} />
+            <Route exact path="/menu" component = { () => <Menu dishes={this.state.dishes} />} />
+            <Redirect to="/home" />
+          </Switch>
           <Footer />
       </div>
     );
