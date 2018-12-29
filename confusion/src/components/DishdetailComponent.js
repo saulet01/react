3b593d74355edcedb5e96179a21383ff7e8c6227
Saulet Yskak <sauletik01@gmail.com>
2018-12-28 21:36:07 -0600

    Updated DishDetail Components

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Moment from 'moment';
-import {Card, CardHeader, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
+import {Card, CardHeader, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderComments({dish}){
   Moment.locale('en');
   if(dish != null){
-    const commentDish = dish.comments.map( (dis) => {
+    const commentDish = dish.map( (dis) => {
       return(
         <div>
           <CardText>{dis.comment}</CardText>
@@ -56,11 +57,21 @@ const DishDetail = (props) => {
   return(
     <div className="container">
       <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>{props.dish.name}</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row">
         <div className="col-12 col-md-5 m-1">
           <RenderDish dish = {props.dish} />
         </div>
         <div className="col-12 col-md-5 m-1">
-          <RenderComments dish = {props.dish} />
+          <RenderComments dish = {props.comments} />
         </div>
       </div>
     </div>
